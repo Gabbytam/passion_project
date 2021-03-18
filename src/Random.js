01e3059f67c07dd6d9d844cd26df307c6aba75df
Gabbytam <gtam2@fordham.edu>
2021-03-17 15:39:06 -1000

    using 2 diff axios calls to first get the number of projects and then get info for just one specific project

--- src/Random.js
+++ src/Random.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import Details from './Details';
 
+const urlStart = 'https://api.globalgiving.org/api/public/projectservice/';
 const api_key = 'bc212140-0729-4c60-a886-a9b73c05ea49';
 
 function Random() {
@@ -15,11 +16,24 @@ function Random() {
     //function that does initial axios call to get projects
     const getProjects = async () => {
         try {
-            let result = await axios.get('https://api.globalgiving.org/api/public/projectservice/all/projects/active?api_key=' + api_key);
-            console.log(result.data)
-            setProjectData(result.data);
-            //call choseRandomProject
-            choseRandomProject();
+            //initial call for 10 can be used for the slot machine
+            // let result = await axios.get(urlStart + 'all/projects/active?api_key=' + api_key);
+            // console.log(result.data)
+            // setProjectData(result.data);
+            // //call choseRandomProject
+            // choseRandomProject();
+
+            //do this call the see how many active projects there are 
+            let result = await axios.get(urlStart + 'all/projects/active/ids?api_key=' + api_key);
+            console.log(result.data.projects)
+            let numberOfProjects = result.data.projects.numberFound;
+            //call choseRandomProject that will return the id of a random project
+            let random = await choseRandomProject(numberOfProjects);
+            //do axios call for a specific project 
+            let newUrl = urlStart +'projects/' + random + '?api_key=' + api_key;
+            let singleResult = await axios.get(newUrl);
+            //console.log('chosen project', singleResult);
+            setChosenProject(singleResult.data.project);
         } 
         catch(err) {
             console.log('axios call for projects error', err)
@@ -27,10 +41,12 @@ function Random() {
     }
 
     //function that will pick a project at random and set the chosenProject
-    const choseRandomProject = () => {
-        let randomNum = Math.floor(Math.random() * 10);
-
+    const choseRandomProject = (num) => {
+        let randomNum = Math.floor(Math.random() * num);
+        return randomNum;
     }
+
+    console.log('chosen project', chosenProject);
 
     return (
         <div>
